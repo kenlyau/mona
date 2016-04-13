5a8ad8b81c1a228482796e5f2add71d005c6ae3a
kenlyau <newallah@gmail.com>
2016-04-13 16:26:28 +0800

    vuejs init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+   <h1 v-touch:swipedown="onSwipeDown">{{msg}}</h1>
+   <p>{{ev}}</p>
+   <comp-a></comp-a>
+   <comp-b></comp-b>
+   <counter></counter>
+</div>
+</template>
+
+<script>
+import CompA from './components/A.vue'
+import CompB from './components/B.vue'
+import Counter from './components/Counter.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue-loader!',
+      ev: '111'
+    }
+  },
+  methods: {
+    onSwipeDown: function (ev) {
+      this.ev = ev
+    }
+  },
+  components: {
+    CompA,
+    CompB,
+    Counter
+  }
+}
+</script>
+
+<style>
+    html,body {padding:0; margin:0;}
+    h1 {color: pink;}
+</style>
