1c178c598484eab6b8e3002e6c872230758e9ad3
vagrant <vagrant@precise32.(none)>
2016-07-06 16:36:27 +0800

    pageslider

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("hello_ webpack...")
+var Zepto = require("../node_modules/zepto/zepto.min");
+var PageSlider =  require("./lib/PageSlider");
+new PageSlider({
+    pages: $(".page-wrap .page")
+});
