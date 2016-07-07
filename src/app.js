5f39ffcf990f94b2a4c47c18a197d563fb7c1882
kenlyau <kenlyau@ken-mac.local>
2016-07-07 23:34:38 +0800

    add base style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,52 @@
-var Zepto = require("../node_modules/zepto/zepto.min");
+var Zepto = require("./lib/zepto");
 var PageSlider =  require("./lib/PageSlider");
+var anime = require("./lib/anime");
+
+var animeObj = {}, index;
+//init slider
 new PageSlider({
-    pages: $(".page-wrap .page")
+    pages: $(".page-wrap .page"),
+    dev:false,
+    oninit: oninit,
+    onbeforechange: onbeforechange,
+    onchange: onchange
 });
+
+//oninit callback
+function oninit(){
+
+};
+
+//onbeforechange callback
+function onbeforechange(){
+
+};
+
+//onchange callback
+function onchange(){
+
+    index = $(".page").index(".current");
+    if (index==1){
+        if (animeObj.one){
+            animeObj.one.restart();
+            return;
+        }
+        animeObj.one = anime({
+            targets: ".current .title",
+            translateX: "13rem",
+            rotate: {
+                value: 80,
+                duration: 1500,
+                easing: "easeInOutQuad"
+            },
+            scale: {
+                value: 2,
+                delay: 150,
+                duration: 850,
+                easing: "easeInOutExpo"
+            },
+            direction: "alternate",
+            loop: true
+        })
+    }
+}
